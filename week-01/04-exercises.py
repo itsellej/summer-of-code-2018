500ffc48d76724c1164b911e361225eeb583e98b
Elle H <itsellej@gmail.com>
2018-07-19 12:05:39 +0100

    Complete deaf grandma

--- week-01/04-exercises.py
+++ week-01/04-exercises.py
@@ -1,16 +1,26 @@
-#WEEK 1 - DAY 4 - LOOPS AND CONDITIONS
-print("")
-print("WEEK 1, DAY 4 EXERCISES:")
+# #WEEK 1 - DAY 4 - LOOPS AND CONDITIONS
+# print("")
+# print("WEEK 1, DAY 4 EXERCISES:")
+#
+# #1. 99 Bottles of Beer on the Wall
+#
+# bottles = 99
+#
+# while bottles > 0:
+#     if bottles > 1:
+#         print(f"{bottles} bottles of beer on the wall")
+#         bottles -=1
+#     else:
+#         print(f"{bottles} bottle of beer on the wall")
+#         break
+# print(f"No more bottles of beer on the wall")
 
-#1. 99 Bottle of Beer on the Wall
+#Deaf grandma
 
-bottles = 99
+talk = input("What would you like to say to Grandma? ")
 
-while bottles > 0:
-    if bottles > 1:
-        print(f"{bottles} bottles of beer on the wall")
-        bottles -=1
-    else:
-        print(f"{bottles} bottle of beer on the wall")
-        break
-print(f"No more bottles of beer on the wall")
+while talk != talk.upper():
+    print("HUH? SPEAK UP, GIRL!")
+    talk = input("What would you like to say to Grandma? ")
+
+print("NO, NOT SINCE 1938!")
