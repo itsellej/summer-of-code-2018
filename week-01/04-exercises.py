9a72d63e3efdde8543b77612d2fc520f9e47b088
Elle H <itsellej@gmail.com>
2018-07-19 13:11:15 +0100

    Compleye deaf grandma extended part 2 exercise

--- week-01/04-exercises.py
+++ week-01/04-exercises.py
@@ -15,7 +15,7 @@
 #         break
 # print(f"No more bottles of beer on the wall")
 
-#Deaf grandma
+#2.Deaf grandma
 
 # talk = input("What would you like to say to Grandma? ")
 #
@@ -26,19 +26,73 @@
 # print("NO, NOT SINCE 1938!")
 
 
-#Deaf grandma extended
+#3. Deaf grandma extended, part 1
+#Grandma shouts different numbers, and doesn't stop talking until you say bye
+
+# from random import randint
+#
+# talk = input("What would you like to say to Grandma? ")
+#
+# while talk != "BYE":
+#     if talk == talk.upper():
+#         random_number = randint(1930, 1950)
+#         print(f"NO, NOT SINCE {random_number}")
+#     else:
+#         print("HUH?! SPEAK UP, GIRL")
+#
+#     talk = input("What would you like to say to Grandma? ")
+#
+# print("GOODBYE DEAR. SEE YOU SOON")
+
+
+#4. Deaf grandma extended, part 2
+#Grandma keeps talking until you say "BYE" three times in a row
 
 from random import randint
 
+bye_count = 0
 talk = input("What would you like to say to Grandma? ")
 
-while talk != "BYE":
-    if talk == talk.upper():
+while bye_count < 3:
+    if talk == talk.upper() and talk !="BYE":
         random_number = randint(1930, 1950)
         print(f"NO, NOT SINCE {random_number}")
+        bye_count = 0
+
+    elif talk == "BYE":
+        bye_count += 1
+
+        if bye_count != 3:
+            print("HUH?! SPEAK UP, GIRL")
+        else:
+            print("GOODBYE DEAR")
+            break
+
     else:
-        print("HUH?! SPEAK UP, GIRL")
+        print("STOP WHISPERING, GIRL!")
+        bye_count = 0
 
     talk = input("What would you like to say to Grandma? ")
 
-print("GOODBYE DEAR. SEE YOU SOON")
+
+
+
+# while talk != "BYE":
+#     bye_count = 0
+#     if talk == talk.upper():
+#         random_number = randint(1930, 1950)
+#         print(f"NO, NOT SINCE {random_number}")
+#         talk = input("What would you like to say to Grandma? ")
+#     else:
+#         print("STOP WHISPERING, GIRL!")
+#         talk = input("What would you like to say to Grandma? ")
+#
+# while talk == "BYE":
+#     bye_count +=1
+#     if bye_count != 3:
+#         print("HUH?! SPEAK UP, GIRL")
+#         talk = input("What would you like to say to Grandma? ")
+#
+#     else:
+#         print("GOODBYE DEAR. SEE YOU SOON")
+#         break
