289f9ba3222802b3931f7734b3ce4415118ff45f
Elle H <itsellej@gmail.com>
2018-07-23 14:48:07 +0100

    Complete week 02 exercise 1: count letter occurance in file

--- week-02/01-exercises.py
+++ week-02/01-exercises.py
@@ -1,9 +1,19 @@
+def count_letters(filename):
 
-def count_letters(book)
+    #open file and read fuke
+    file = open(filename, "r")
+    read_book = file.read()
 
-from collections import count_letters
+    from collections import Counter
 
-#set open filename to be read to a variable
-#book
+    #Create dictionary using letter as key and quantity as value pair
+    characters = Counter(read_book)
 
-print(Counter(book))
+    #sort the dictionary by key
+    # if the key is alphabetic, print key value pair
+    for key, value in sorted(characters.items()):
+        if key.isalpha():
+            print(key, value)
+
+count_letters("01-test2.txt")
+count_letters("01-test.txt")
