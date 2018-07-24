a375f40a3499d036faa2f3adf5d238d36b17ad02
Elle H <itsellej@gmail.com>
2018-07-24 16:48:42 +0100

    Complete question 1 - bug fix

--- week-02/02-exercises.py
+++ week-02/02-exercises.py
@@ -1,7 +1,36 @@
-#1. ASCII, print A-Z and a-z only
+# #1. There is something small that needs fixing. Can you spot it and fix it? (Hint, we only want A-Z and a-z)
+#
+# for i in range(65,123):
+#     if chr(i).isalpha():
+#         print(i, " stands for ", chr(i))
+#
+# #2. Make a function that prints A-Z and a-z
 
-#2. Make a function that prints A-Z and a-z
+# def alpha():
+#     for i in range(65,123):
+#         if chr(i).isalpha():
+#             print(i, " stands for ", chr(i))
+#
+# #test
+# alpha()
 
-#3. Make a function that askss the user for a message, and turns it into a list of numbers - cypher
+#3. Make a function that asks the user for a message, and turns it into a list of numbers. (It's a cypher )
+
+def cypher(message):
+    letters = list(message)
+    print(letters)
+
+    decoded = []
+    for i in letters:
+    #     if i == " ":
+    #         decoded.append(" ")
+    # else:
+        decoded.append(ord(i))
+
+
+    print(decoded)
+
+#test
+cypher("I LOVE YOU")
 
 #4. Optional - caeser cypher
