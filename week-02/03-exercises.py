62c3286847f418dc5dcd8ab23b9309c9a43144c5
Elle H <itsellej@gmail.com>
2018-07-25 16:05:04 +0100

    Complete Q1 - modify a in dictionary

---
+++ week-02/03-exercises.py
@@ -0,0 +1,16 @@
+#1. Modify "a" for another name in my_dict
+
+my_dict = {
+    "a": 35000,
+    "b": 8000,
+    "z": 450
+}
+
+my_dict["a_new"] = 35000
+
+del(my_dict["a"])
+
+print(my_dict)
+
+
+#2. Redo the frequency distribution of alice_in_wonderland.txt and save your result in a dictionary
