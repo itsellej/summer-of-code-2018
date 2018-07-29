d4f5472a4497c0a84c2171986b48557756aede90
Elle H <itsellej@gmail.com>
2018-07-29 14:28:40 +0100

    Complete Q1 of day 4 exercises

---
+++ week-02/04-exercises.py
@@ -0,0 +1,8 @@
+#From http://www.nltk.org/book/ch01.html
+
+#Q1. Compare the lexical diversity scores for humor and romance fiction in 1.1. Which genre is more lexically diverse?
+
+# Humor - lexical diversity = 0.231 (23%)
+# Romance fiction - lexical diversity = 0.121 (12%)
+
+#Therefore the number of distinct words for Humor is approximately 23% of the total number of words, and is more lexically diverse
